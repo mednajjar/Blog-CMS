f93bd0f4fb224f13c65e21637d3a70c2b0a098ed
Mohammed Najjar <najjarmedd@gmail.com>
2020-07-05 06:11:23 +0100

    Handling Cookies

---
+++ vendor/System/Cookie.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace System;
+
+class Cookie
+{
+    /**
+     * Application Object
+     * 
+     * @var \System\Application
+     */
+    private $app;
+    /**
+     * Constructor
+     * 
+     * @param \System\Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app =$app;
+    }
+  
+    /**
+     * Set New Value to Cookie
+     * 
+     * @param string $key
+     * @param mixed $value
+     * @param int $minutes
+     * @return void
+     */
+    public function set($key, $value, $hours = 1800)
+    {
+        setcookie($key, $value, time() + $hours * 3600, '', '',  false, true);
+    }
+    /**
+     * Get value from Cookie by the given key
+     * 
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        return array_get($_COOKIE, $key, $default);
+    }
+    /**
+     * Determin if the Cookie has the given key
+     * 
+     * @param string $key
+     * @return bool
+     */
+    public function has($key)
+    {
+        return array_key_eists($key, $_COOKIE);
+    }
+    /**
+     * Remove the given key from Cookie
+     * 
+     * @param string $key
+     * @return void
+     */
+    public function remove($key)
+    {
+        setcookie($key, null, -1);
+
+        unset($_COOKIE[$key]);
+    }
+  
+    /**
+     * Get all Cookie data
+     * 
+     * @return array
+     */
+    public function all()
+    {
+        return $_COOKIE;
+    }
+    /**
+     * Destroy Cookie
+     * 
+     * @return void
+     */
+    public function destroy()
+    {
+        foreach(array_keys($this->all())as $key)
+        {
+            $this->remove($key);
+        }
+        unset($_COOKIE);
+    }
+}
+
+?>
